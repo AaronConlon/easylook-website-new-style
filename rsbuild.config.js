8518aec146db5a981dad03eb2e52961410c1e62f
AaronConlon <rivenqinyy@gmail.com>
2026-01-15 19:03:39 +0800

    feat: 增加 seo 优化

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -24,5 +24,20 @@ export default defineConfig({
   },
   html: {
     favicon: './public/favicon.svg',
+    title: '视立优 EASYLOOK - 专业的视力保护解决方案提供商',
+    meta: {
+      description:
+        '视立优 EASYLOOK 专业的视力保护解决方案提供商，深耕于眼视光医疗行业，致力于为大众提供近视防控、行为视光、视觉康复、成人视疲劳等各类眼视光前沿性产品。',
+      'og:title': '视立优 EASYLOOK - 专业的视力保护解决方案提供商',
+      'og:description':
+        '视立优 EASYLOOK 专业的视力保护解决方案提供商，深耕于眼视光医疗行业，致力于为大众提供近视防控、行为视光、视觉康复、成人视疲劳等各类眼视光前沿性产品。',
+      'og:image': '/company.jpg',
+      'og:type': 'website',
+      'twitter:card': 'summary_large_image',
+      'twitter:title': '视立优 EASYLOOK - 专业的视力保护解决方案提供商',
+      'twitter:description':
+        '视立优 EASYLOOK 专业的视力保护解决方案提供商，深耕于眼视光医疗行业，致力于为大众提供近视防控、行为视光、视觉康复、成人视疲劳等各类眼视光前沿性产品。',
+      'twitter:image': '/company.jpg',
+    },
   },
 });
